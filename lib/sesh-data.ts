85c531ceafc41d249ccdd9013a27e19c524f954b
Tom James <tom@soberlifeco.co.uk>
2026-09-21 19:05:09 +0000

    Add Sesh 51 to the leaderboard

--- lib/sesh-data.ts
+++ lib/sesh-data.ts
@@ -61,6 +61,7 @@ export const SESH_DATA: Sesh[] = [
   { sesh: 48, music: 0, substances: 3, cat3: 5, hangover: 2, cat5: 5, total: 15, maxScore: 25, instagram: "https://www.instagram.com/soberlifetom/reel/Ddb9EkANdMG/" },
   { sesh: 49, music: 5, substances: 4, cat3: 0, hangover: 1, cat5: 2, total: 12, maxScore: 25, instagram: "https://www.instagram.com/soberlifetom/reel/DdeaqeqtvBN/" },
   { sesh: 50, music: 5, substances: 2, cat3: 3, hangover: 4, cat5: 5, total: 19, maxScore: 25, instagram: "https://www.instagram.com/soberlifetom/reel/Ddg6R9YN0yj/" },
+  { sesh: 51, music: 5, substances: 2, cat3: 2, hangover: 3, cat5: 5, total: 17, maxScore: 25, instagram: "https://www.instagram.com/soberlifetom/reel/DdjmQ9UNcPs/" },
 ];
 
 export const RANKED = [...SESH_DATA].sort((a, b) => b.total - a.total || a.sesh - b.sesh);
